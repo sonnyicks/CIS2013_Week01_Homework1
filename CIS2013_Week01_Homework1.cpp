63641f6def58d149fef4c2bf41e067a45989d477
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-01-22 15:24:03 -0600

    Add files via upload

---
+++ CIS2013_Week01_Homework1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	cout << "==================================================\n";
+	cout << "\n";
+	cout << " Sonny Icks, COT, CCRC \n";
+	cout << " Clinical Research Coordinator \n";
+	cout << "\n";
+	cout << "                              phone: 405-248-6327 \n";
+	cout << "                    email: sonny.icks@okstate.edu \n";
+	cout << "                     github: github.com/sonnyicks \n";
+	cout << "\n";
+	cout << "==================================================\n";
+	
+	return 0;
+}
